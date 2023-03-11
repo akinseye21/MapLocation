90db173a803ae491ea110cc9b4b4e59da31ad6a8
Akinseye Abayomi <abayomi.akinseye@gmail.com>
2023-03-11 23:06:29 +0100

    Initial Commit

---
+++ SwiftMapApp/SwiftMapAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftMapAppApp.swift
+//  SwiftMapApp
+//
+//  Created by Yomi on 3/11/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftMapAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
